9a5653e93eb128e2c52dea597e87c202b26bec2e
John U. Balis <phobossystems@gmail.com>
2021-11-12 05:24:46 -0600

    added basic json logging, and blocking until new policy can be loaded. Still needs blocking for initial policy load, and policy re-loading and deletion from filesystem.

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/CodeReleaseKickAtGoalCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/CodeReleaseKickAtGoalCard.cpp
@@ -14,6 +14,8 @@
 #include <iostream>
 #include <math.h>
 #include <unistd.h>
+#include <string>
+
 
 #include "Representations/BehaviorControl/FieldBall.h"
 #include "Representations/BehaviorControl/Skills.h"
@@ -38,9 +40,151 @@
 #define STATS_ENABLE_EIGEN_WRAPPERS
 #define STATS_ENABLE_STDVEC_WRAPPERS
 #include "Tools/NeuralNetwork/stats.hpp"
+#include <fstream>
 
 
 #define PI 3.14159265
+#define BATCHSIZE 20
+#define DEBUG_MODE true
+
+
+int batchStep  = 0; // frame index number within batch we are on
+int episodeStep = -1; // the episode needs to complete one step before we can start logging
+
+
+      
+
+auto trajectories = json::object{}; // an object to hold the curret batch of trajectories
+std::ifstream metadataFile("../metadata.json");
+json::value metadata = json::parse(metadataFile);
+std::string actionLengthString = to_string(metadata["action_length"]);
+unsigned int actionLength = (unsigned int)(std::stoi(actionLengthString));
+std::string observationLengthString = to_string(metadata["observation_length"]);
+unsigned int observationLength = (unsigned int)(std::stoi(observationLengthString));
+std::vector<float> prevObservation;
+std::vector<float> currentObservation;
+int prevDone = 1; // whether this frame is the first frame of an episode or not
+std::vector<float> prevAction;
+std::vector<float> currentAction;
+double prevLogProb;
+double currentLogProb;
+float currentValue;
+float prevValue;
+
+
+ 
+
+std::string getCurrentDirectory()
+{
+  char buff[FILENAME_MAX]; //create string buffer to hold path
+  getcwd( buff, FILENAME_MAX );
+  std::string currentWorkingDir(buff);
+  return currentWorkingDir;
+}
+
+
+
+void debugPrintString(std::string s){
+  if(DEBUG_MODE){
+  std::cout << s << std::endl;
+  }
+}
+
+
+void debugPrintFloatVector(std::vector<float> v){
+  if (DEBUG_MODE)
+  {
+  for (auto i : v)
+  {
+    std::cout << i << std::endl;
+  }
+  }
+}
+
+
+// derived from https://stackoverflow.com/questions/12774207/fastest-way-to-check-if-a-file-exist-using-standard-c-c11-14-17-c
+
+
+  bool doesFileExist (const std::string& name) {
+    std::ifstream f(name.c_str());
+    return f.good();
+  }
+
+
+void waitForNewPolicy()
+{
+  while (true)
+  {
+    if (!doesFileExist(getCurrentDirectory() + "/../shared_policy.h5"))
+    {
+      debugPrintString("waiting for shared policy");
+      continue;
+    }
+    if (!doesFileExist(getCurrentDirectory() +"/../action_policy.h5"))
+    {
+      debugPrintString("waiting for action policy");
+      continue;
+    }
+    if (!doesFileExist(getCurrentDirectory() +"/../value_policy.h5"))
+    {
+      debugPrintString("waiting for value policy");
+      continue;
+    }
+    if (!doesFileExist(getCurrentDirectory() +"/../metadata.json"))
+    {
+      debugPrintString("waiting for meta data");
+      continue;
+    }
+    break;
+  }
+}
+
+
+
+
+json::object newTrajectoriesJSON()
+{
+        json::object emptyTrajectories = json::object{};
+        emptyTrajectories.insert("last_values", json::array{});
+        emptyTrajectories.insert("length", BATCHSIZE);
+        emptyTrajectories.insert("observations", json::array{});
+        emptyTrajectories.insert("episode_starts", json::array{});
+        emptyTrajectories.insert("values",json::array{});
+        emptyTrajectories.insert("actions",json::array{});
+        emptyTrajectories.insert("log_probs", json::array{});
+        return emptyTrajectories;
+}
+
+json::array floatVectToJSON(std::vector<float> inputVector)
+{
+   
+        json::array output = json::array{};
+        for (float i: inputVector)
+        {
+          output.push_back(i);
+          //std::cout << i << std::endl;
+        }
+        return output;
+}
+
+
+
+// returns an observation from the environment as a vector
+std::vector<float> getObservation(GroundTruthRobotPose pose)
+{
+  std::vector<float> observationVector(4); // observation length is hardcoded for the time being
+  double x = pose.translation[0];
+  double y = pose.translation[1];
+  double angle = pose.rotation;
+  double sinAngle = sin(angle* (PI/180));
+  double cosAngle = cos(angle* (PI/180));
+  observationVector[0] = x/4500.0;
+  observationVector[1] = y/3000.0;
+  observationVector[2] = sinAngle;
+  observationVector[3] = cosAngle;
+  return observationVector;
+}
+
 
 CARD(CodeReleaseKickAtGoalCard,
 {,
@@ -104,39 +248,65 @@ class CodeReleaseKickAtGoalCard : public CodeReleaseKickAtGoalCardBase
         {
 
 
-        if(Blackboard::getInstance().exists("GroundTruthRobotPose")){
-        //std::cout << "exists" << std::endl;
-        const GroundTruthRobotPose& theGroundTruthRobotPose = static_cast<const GroundTruthRobotPose&>(Blackboard::getInstance()["GroundTruthRobotPose"]); \
-        double x = theGroundTruthRobotPose.translation[0];
-        double y = theGroundTruthRobotPose.translation[1];
-        double angle = theGroundTruthRobotPose.rotation;
-
-        /*std::cout<< "x" << std::endl;
-        std::cout << x << std::endl;
-        std::cout<< "y" << std::endl;
-        std::cout << y << std::endl;
-        std::cout<< "angle" << std::endl;
-        std::cout << angle << std::endl;
-         */
-        double sinAngle = sin(angle* (PI/180));
-        double cosAngle = cos(angle* (PI/180));
+       if(Blackboard::getInstance().exists("GroundTruthRobotPose")){
+       const GroundTruthRobotPose& theGroundTruthRobotPose = static_cast<const GroundTruthRobotPose&>(Blackboard::getInstance()["GroundTruthRobotPose"]); 
 
 
-        char buff[FILENAME_MAX]; //create string buffer to hold path
-        getcwd( buff, FILENAME_MAX );
-        std::string currentWorkingDir(buff);
-        //std::cout << currentWorkingDir << std::endl;
-                
-        //std::ifstream metadataFile("/home/john/BHumanCodeRelease/Config/NeuralNets/metadata.json");
-        std::ifstream metadataFile("../../Config/NeuralNets/metadata.json");
 
-        json::value metadata = json::parse(metadataFile);
+        NeuralNetwork::CompilationSettings settings;  // not sure if this is necessary but it's unused since we're not compiling but was required
+        // for apply function signature
+       
         
-        std::string observationLengthString = to_string(metadata["observation_length"]);
-        unsigned int observationLength = (unsigned int)(std::stoi(observationLengthString));
+       
+        NeuralNetwork::Model sharedModel("shared_policy.h5");
+        NeuralNetwork::Model actionModel("action_policy.h5");
+        NeuralNetwork::Model valueModel("value_policy.h5");
 
-        std::string actionLengthString = to_string(metadata["action_length"]);
-        unsigned int actionLength = (unsigned int)(std::stoi(actionLengthString));
+        
+
+
+        SimRobotCore2::Scene* scene = (SimRobotCore2::Scene*)RoboCupCtrl::application->resolveObject("RoboCup", SimRobotCore2::scene);
+
+        /*
+        if (scene != NULL) {
+          episodeStep = scene->getStep();
+        }
+        else{
+          std::cout << "FATAL ERROR: scene is NULL" << std::endl;
+          exit(1);
+        }
+        */
+        currentObservation = getObservation(theGroundTruthRobotPose);
+
+
+
+        if (episodeStep == 0)
+        {
+          prevDone = true;
+        }
+        else if(episodeStep > 0){
+          prevDone = false;
+        }
+
+
+        if (batchStep == 0) // setting up our first trajectories file descriptor, this will be exchanged on each 
+        {
+          trajectories = newTrajectoriesJSON();
+        }
+  
+
+       
+
+        //outputFile << "test" << '\n';
+
+        
+        //std::cout << "exists" << std::endl;
+        
+
+
+
+        //std::ifstream metadataFile("/home/john/BHumanCodeRelease/Config/NeuralNets/metadata.json");
+        
 
 
         /*std::cout <<  "OBSERVATION LENGTH" << std::endl;
@@ -179,42 +349,43 @@ class CodeReleaseKickAtGoalCard : public CodeReleaseKickAtGoalCardBase
         
 
 
-        NeuralNetwork::CompilationSettings settings;  // not sure if this is necessary but it's unused since we're not compiling but was required
-        // for apply function signature
-        NeuralNetwork::Model sharedModel("NeuralNets/shared_policy.h5");
-        NeuralNetwork::Model actionModel("NeuralNets/action_policy.h5");
-        NeuralNetwork::Model valueModel("NeuralNets/value_policy.h5");
-
-        
+     
     
         
 
         std::vector<NeuralNetwork::TensorXf> sharedOutputs(sharedModel.getOutputs().size());
 
-        std::vector<NeuralNetwork::TensorXf> observation(sharedModel.getInputs().size());
+        std::vector<NeuralNetwork::TensorXf> observationInput(sharedModel.getInputs().size());
         //std::cout << "policy load and input setup complete" << std::endl;
         //reshaping but not sure why, derived from check.cpp
         const std::vector<NeuralNetwork::TensorLocation>& inputs = sharedModel.getInputs();
-        for(std::size_t i = 0; i < observation.size(); ++i)
+        for(std::size_t i = 0; i < observationInput.size(); ++i)
         {
-          observation[i].reshape(inputs[i].layer->nodes[inputs[i].nodeIndex].outputDimensions[inputs[i].tensorIndex]);
+          observationInput[i].reshape(inputs[i].layer->nodes[inputs[i].nodeIndex].outputDimensions[inputs[i].tensorIndex]);
         }
 
 
 
-        observation[0][0] = x;
-        observation[0][1] = y;
-        observation[0][2] = sinAngle;
-        observation[0][3] = cosAngle;
+        observationInput[0][0] = currentObservation[0];
+        observationInput[0][1] = currentObservation[1];
+        observationInput[0][2] = currentObservation[2];
+        observationInput[0][3] = currentObservation[3];
 
         //std::cout << "OBSERVATION :" << std::endl;
-        for (float i: observation[0])
+
+        /*
+        auto newObs = json::array{};
+        for (float i: observationInput[0])
         {
+          newObs.push_back(i);
           //std::cout << i << std::endl;
         }
-       
+        trajectories["observations"].push_back(newObs);
+        */
+        
+        
         //std::cout << "reached pre apply" << std::endl;
-        NeuralNetwork::SimpleNN::apply(observation, sharedOutputs, sharedModel, [&settings](const NeuralNetwork::Node& node, const std::vector<const NeuralNetwork::TensorXf*>& inputs, const std::vector<NeuralNetwork::TensorXf*>& outputs)
+        NeuralNetwork::SimpleNN::apply(observationInput, sharedOutputs, sharedModel, [&settings](const NeuralNetwork::Node& node, const std::vector<const NeuralNetwork::TensorXf*>& inputs, const std::vector<NeuralNetwork::TensorXf*>& outputs)
         {
         });
 
@@ -273,7 +444,9 @@ class CodeReleaseKickAtGoalCard : public CodeReleaseKickAtGoalCardBase
         {
           //std::cout << i << std::endl;
         }
-      
+        //trajectories["values"].push_back(valueEstimate(0));
+        currentValue = valueEstimate(0);
+
 
         std::vector<NeuralNetwork::TensorXf> actionPolicyInput(actionModel.getInputs().size());
         actionPolicyInput[0] = latentAction;
@@ -315,24 +488,94 @@ class CodeReleaseKickAtGoalCard : public CodeReleaseKickAtGoalCardBase
 
         //std::cout << actionChoice << std::endl;
 
-        double logProb = stats::dmvnorm(actionChoice,actionEigen, covarianceMatrix, true);
+        currentLogProb = stats::dmvnorm(actionChoice,actionEigen, covarianceMatrix, true);
 
+
+
+        //trajectories["log_probs"].push_back(logProb);
         //std::cout << "action log prob: " << std::endl;
 
         //std::cout << logProb << std::endl;
 
-      
+        currentAction = std::vector<float>();
+        for (unsigned int i = 0; i < actionChoice.size(); i++)
+        {
+          currentAction.push_back(actionChoice(i));
+        }
+
         
+
+        if (episodeStep > -1)
+        {
+          trajectories["episode_starts"].push_back(prevDone);
+          trajectories["observations"].push_back(floatVectToJSON(prevObservation));
+          trajectories["actions"].push_back(floatVectToJSON(prevAction));
+          trajectories["values"].push_back(prevValue);
+          trajectories["last_values"].push_back(currentValue);
+          trajectories["log_probs"].push_back(prevLogProb);
+
+
+
+          debugPrintFloatVector(prevObservation);
+          debugPrintFloatVector(currentObservation);
+          std::cout << "open" << std::endl;
+          std::cout << currentValue << std::endl;
+          std::cout << prevValue << std::endl;
+          std::cout << "close" << std::endl;
+
+          //log here
+        }
+
+
+        //logging here
+
+
+
+
+          std::cout<<episodeStep<<std::endl;            
+          
+         
+
+        
+        
+        
+        
+        if (episodeStep > -1)
+        {
+          batchStep += 1;
+        }
+        episodeStep += 1;
+
+
+        if (episodeStep >= 10) {
+          std::cout << stringify(trajectories, json::PrettyPrint) << std::endl;
+          RoboCupCtrl::application->resetSimulation();
+          episodeStep = 0;
+          if (batchStep >= BATCHSIZE)
+          {
+            waitForNewPolicy();
+            //load new policy
+            //delete new policy from filesystem
+            trajectories = newTrajectoriesJSON();
+          }
+          
+        }
+      
+        prevObservation = std::vector<float>(currentObservation);
+        prevAction = std::vector<float>(currentAction);
+        prevValue = currentValue;
+        prevLogProb = currentLogProb;
+
         theWalkAtRelativeSpeedSkill(Pose2f(actionChoice(0),actionChoice(1), actionChoice(2)));
 
-          SimRobotCore2::Scene* scene = (SimRobotCore2::Scene*)RoboCupCtrl::application->resolveObject("RoboCup", SimRobotCore2::scene);
 
-          if (scene != NULL) {
-            std::cout<<scene->getStep()<<std::endl;
-            if (scene -> getStep() >= 1000) {
-              RoboCupCtrl::application->resetSimulation();
-            }
-          }
+
+        //currentObservation = getObservation(theGroundTruthRobotPose);
+        //debugPrintFloatVector(prevObservation);
+        //debugPrintFloatVector(currentObservation);
+        //exit(1);
+
+
         }
         else{
        
