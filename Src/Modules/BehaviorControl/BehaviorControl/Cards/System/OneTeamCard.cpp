d743432df0f86d2f07f6b16d513dbfa777f99ca8
yourlucky <yoonchae.na@gmail.com>
2022-03-15 00:00:40 -0500

    oneTeamCard.cpp

---
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/System/OneTeamCard.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file NoTeamCard.cpp
+ *
+ * This file implements card that is active in situations where there is no team behavior.
+ *
+ * @author Arne Hasselbring
+ */
+
+#include "Representations/BehaviorControl/TeamSkills.h"
+#include "Tools/BehaviorControl/Framework/Card/TeamCard.h"
+
+TEAM_CARD(OneTeamCard,
+{,
+  CALLS(Role),
+  CALLS(TeamActivity),
+  CALLS(TeammateRoles),
+  CALLS(TimeToReachBall),
+});
+
+class OneTeamCard : public OneTeamCardBase
+{
+  bool preconditions() const override
+  {
+    return true;
+  }
+
+  bool postconditions() const override
+  {
+    return true;
+  }
+
+  void execute() override
+  {
+    theTeamActivitySkill(TeamBehaviorStatus::noTeam);
+    theTimeToReachBallSkill(TimeToReachBall());
+    theTeammateRolesSkill(TeammateRoles());
+
+    Role roleone;
+    roleone.isGoalkeeper = false;//initial : false
+    roleone.playBall = true;//initial : false
+    roleone.supporterIndex = -2; //initial : -1
+    roleone.numOfActiveSupporters = 1;
+    theRoleSkill(roleone);
+  }
+};
+
+MAKE_TEAM_CARD(OneTeamCard);
