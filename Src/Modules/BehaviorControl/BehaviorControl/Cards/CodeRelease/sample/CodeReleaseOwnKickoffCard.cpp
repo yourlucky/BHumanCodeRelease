abe97d02e3eb839779657704eae2ff7aede0a555
yourlucky <yoonchae.na@gmail.com>
2022-03-18 11:28:43 -0500

    go

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/sample/CodeReleaseOwnKickoffCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/sample/CodeReleaseOwnKickoffCard.cpp
@@ -40,15 +40,5 @@ class CodeReleaseOwnKickoffCard : public CodeReleaseOwnKickoffCardBase
     theSaySkill("yes Goal Own Kick off card");
   }
 };
-<<<<<<< HEAD
-<<<<<<< HEAD
-//git fixing
-=======
-//fix git
-//fix git2
->>>>>>> 54e00088be1ef0d3d4c336210eb69c3700cc1d2f
-=======
-//fix git
-//fix git2
->>>>>>> 54e00088 (new git)
+
 MAKE_CARD(CodeReleaseOwnKickoffCard);
