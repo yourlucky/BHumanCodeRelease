54514befba2e3fdcf964ea44f0411beb167b6e2e
yourlucky <yoonchae.na@gmail.com>
2022-04-03 01:01:34 -0500

    conflicy

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/CodeReleaseKickAtGoalCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/CodeReleaseKickAtGoalCard.cpp
@@ -129,7 +129,7 @@ class CodeReleaseKickAtGoalCard : public CodeReleaseKickAtGoalCardBase
       action
       {
         //const Vector2f _ballPosition = theGroundTruthWorldState.balls[0].position.head<2>();
-        const Vector2f _ballPosition = theGroundTruthWorldState.balls[0];
+        const Vector2f _ballPosition = theGroundTruthWorldState.balls[0].position;
         
         //Vector2f _ownPosition = theGroundTruthWorldState.ownPose[0].position.head<2>();  
 
