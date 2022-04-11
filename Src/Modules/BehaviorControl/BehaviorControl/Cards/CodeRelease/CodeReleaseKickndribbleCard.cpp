d48dbc8f6e67070119975c361d93a9f380024fcb
yourlucky <yoonchae.na@gmail.com>
2022-04-10 22:53:30 -0500

    align2 parameter

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/CodeReleaseKickndribbleCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/CodeRelease/CodeReleaseKickndribbleCard.cpp
@@ -45,14 +45,14 @@ CARD(CodeReleaseKickndribbleCard,
     (int)(7000) ballNotSeenTimeout,
     (Angle)(5_deg) ballAlignThreshold,
     (float)(500.f) ballNearThreshold,
-    (Angle)(10_deg) angleToGoalThreshold,
+    (Angle)(20_deg) angleToGoalThreshold,
     (float)(400.f) ballAlignOffsetX,
     (float)(100.f) ballYThreshold,
     (Angle)(2_deg) angleToGoalThresholdPrecise,
     (float)(150.f) ballOffsetX,
     (Rangef)({140.f, 170.f}) ballOffsetXRange,
-    (float)(40.f) ballOffsetY,
-    (Rangef)({20.f, 50.f}) ballOffsetYRange,
+    (float)(100.f) ballOffsetY,
+    (Rangef)({-20.f, 500.f}) ballOffsetYRange,
     (int)(10) minKickWaitTime,
     (int)(3000) maxKickWaitTime,
     
@@ -60,6 +60,8 @@ CARD(CodeReleaseKickndribbleCard,
 
     (float)(0.f)ball_X,
     (float)(0.f)ball_Y,
+    
+    (int)(-20) c_time,
   }),
 });
 
@@ -102,7 +104,6 @@ class CodeReleaseKickndribbleCard : public CodeReleaseKickndribbleCardBase
         {
           if(theRobotInfo.number == 1)
           {
-            int c_time = 5000; //current time            
             if (state_time > c_time + 5000) //if not fallen for 10secs
             {
               c_time = state_time;
@@ -173,17 +174,18 @@ class CodeReleaseKickndribbleCard : public CodeReleaseKickndribbleCardBase
           
         const Angle v_angle=(theRobotPose.inversePose * Vector2f(_firstteam.translation.x(),_firstteam.translation.y())).angle();
         theLookForwardSkill();
-        theInWalkKickSkill(WalkKickVariant(WalkKicks::forward, Legs::left), Pose2f(v_angle, 0.f, 0.f));
+        theInWalkKickSkill(WalkKickVariant(WalkKicks::forward, Legs::left), Pose2f(v_angle, theFieldBall.positionRelative.x() - ballOffsetX, theFieldBall.positionRelative.y() - ballOffsetY));
       }
     }
 
     state(alignkick)
     {
+     //const Angle k_angle =180;
       transition
       {
         if(!theFieldBall.ballWasSeen(ballNotSeenTimeout))
-          goto searchForBall;
-        if(std::abs(angleToGoal) < angleToGoalThreshold && std::abs(theFieldBall.positionRelative.y()) < ballYThreshold)
+          goto giverole;
+        if(std::abs(theFieldBall.positionRelative.y()) < ballYThreshold)
           goto alignBehindBall;
       }
 
@@ -199,27 +201,17 @@ class CodeReleaseKickndribbleCard : public CodeReleaseKickndribbleCardBase
         ball_X = std::abs(_ownPosition.translation.x()-_ballPosition(0));
         ball_Y =  std::abs(_ownPosition.translation.y()-_ballPosition(1));
         
-        const Angle v_angle=(theRobotPose.inversePose * Vector2f(_firstteam.translation.x(),_firstteam.translation.y())).angle();
+        const Angle v_angle =(theRobotPose.inversePose * Vector2f(_firstteam.translation.x(),_firstteam.translation.y())).angle();
 
 
-        theSaySkill("align to goal");
+        theSaySkill("align first");
           theLookForwardSkill();
-        theWalkToTargetSkill(Pose2f(walkSpeed, walkSpeed, walkSpeed), Pose2f(angleToGoal, theFieldBall.positionRelative.x() - ballAlignOffsetX, theFieldBall.positionRelative.y()));
+        theWalkToTargetSkill(Pose2f(walkSpeed, walkSpeed, walkSpeed), Pose2f(v_angle, theFieldBall.positionRelative.x() - ballAlignOffsetX, theFieldBall.positionRelative.y()));
       }
     }
 
     state(alignBehindBall)
     {
-      transition
-      {
-        if(!theFieldBall.ballWasSeen(ballNotSeenTimeout))
-          goto searchForBall;
-        if(std::abs(angleToGoal) < angleToGoalThresholdPrecise && ballOffsetXRange.isInside(theFieldBall.positionRelative.x()) && ballOffsetYRange.isInside(theFieldBall.positionRelative.y()))
-          goto kick;
-      }
-
-      action
-      {
         const GroundTruthWorldState&theGroundTruthWorldState =
         static_cast<const GroundTruthWorldState&>(Blackboard::getInstance()["GroundTruthWorldState"]);
         
@@ -227,12 +219,23 @@ class CodeReleaseKickndribbleCard : public CodeReleaseKickndribbleCardBase
         const Vector2f _ballPosition = theGroundTruthWorldState.balls[0].position.head<2>();
         const Pose2f _firstteam = theGroundTruthWorldState.firstTeamPlayers[0].pose;
         
-        ball_X = std::abs(_ownPosition.translation.x()-_ballPosition(0));
-        ball_Y =  std::abs(_ownPosition.translation.y()-_ballPosition(1));
+        int I_X =  std::abs(_firstteam.translation.x()-_ownPosition.translation.x());
+        int I_Y = std::abs( _firstteam.translation.y()-_ownPosition.translation.y());
         
+      transition
+      {
+        if(!theFieldBall.ballWasSeen(ballNotSeenTimeout))
+          goto giverole;
+       if(I_X+ I_Y > 2000  && ballOffsetYRange.isInside(theFieldBall.positionRelative.y()))
+          goto kick;
+      }
+
+      action
+      {        
         const Angle v_angle=(theRobotPose.inversePose * Vector2f(_firstteam.translation.x(),_firstteam.translation.y())).angle();
         theLookForwardSkill();
-        theWalkToTargetSkill(Pose2f(walkSpeed, walkSpeed, walkSpeed), Pose2f(angleToGoal, theFieldBall.positionRelative.x() - ballOffsetX, theFieldBall.positionRelative.y() - ballOffsetY));
+        theSaySkill("align second");
+        theWalkToTargetSkill(Pose2f(walkSpeed, walkSpeed, walkSpeed), Pose2f(v_angle, theFieldBall.positionRelative.x() - ballOffsetX, theFieldBall.positionRelative.y() - ballOffsetY));
       }
     }
 
