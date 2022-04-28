9a3f478d47fa87c2c6f6b824114533c26acc611b
yourlucky <yoonchae.na@gmail.com>
2022-04-28 16:20:53 -0500

    Gameplaycard setting

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/System/GameplayCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/System/GameplayCard.cpp
@@ -87,8 +87,8 @@ class GameplayCard : public GameplayCardBase
         {
           case 1: {dealer.deal(twoPlay)->call();setState("normalPlay");}break;
           //case 2: {dealer.deal(ownFreeKick)->call();setState("normalPlay");}break;
+          case 2: {dealer.deal(twoPlay)->call();setState("normalPlay");}break;
           case 3: {dealer.deal(onePlay)->call();setState("normalPlay");}break;
-          case 4: {dealer.deal(normalPlay)->call();setState("normalPlay");}break;
                     //{dealer.deal(FrontDefender)->cal();setState("normalPlay");}break;
           //case 5: {dealer.deal(normalPlay)->call();setState("normalPlay");}break;
                     //{dealer.deal(BackDefender)->cal();setState("normalPlay");}break;
